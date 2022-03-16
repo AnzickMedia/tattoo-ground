53e9402482dd13cd766b8e4912c0929fc5425f09
edsson cortes <edsson_andree.cortes@uao.edu.co>
2022-03-16 15:02:56 -0500

    Footer links agregados

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Footer.scss'
+import { FaFacebookF, FaInstagram, FaTwitter,FaLinkedin } from 'react-icons/fa';
+
 
 const Footer = () => {
   return (
@@ -14,10 +16,24 @@ const Footer = () => {
         </div>
         <div className="o-follow-us">
         <h2>FOLLOW US</h2>
-          <h4>Lorem ipsum dolor sit.</h4>
-          <h4>Lorem ipsum dolor sit.</h4>
-          <h4>Lorem ipsum dolor sit.</h4>
-          <h4>Lorem ipsum dolor sit.</h4>   
+        <div className="o-social-container">
+          <FaFacebookF color='white'/>
+        <h4>Facebook</h4>
+        </div>
+        <div className="o-social-container">
+          <FaInstagram color='white'/>
+        <h4>Instagram</h4>
+        </div>
+        <div className="o-social-container">
+          <FaTwitter color='white'/>
+        <h4>Twitter</h4>
+        </div>
+        <div className="o-social-container">
+          <FaLinkedin color='white'/>
+        <h4>LinkeIn</h4>
+        </div>
+          
+            
         </div>
       </div>
       <div className="o-footer-right">
