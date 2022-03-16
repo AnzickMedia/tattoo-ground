fe7cbffb7756d3dc46ea56c6c4fea06ba23d22a4
edsson cortes <edsson_andree.cortes@uao.edu.co>
2022-03-16 15:11:22 -0500

    Redireccion de links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,20 @@ import { FaFacebookF, FaInstagram, FaTwitter,FaLinkedin } from 'react-icons/fa';
 
 
 const Footer = () => {
+
+  const redirectFace = ()=>{
+    window.open("https://www.facebook.com", "_blank")
+  }
+  const redirectInst = ()=>{
+    window.open("https://www.instagram.com", "_blank")
+  }
+  const redirectTiwt = ()=>{
+    window.open("https://www.twitter.com", "_blank")
+  }
+  const redirectLinked = ()=>{
+    window.open("https://www.linkedin.com", "_blank")
+  }
+
   return (
     <div className='o-footer'>
       <div className="o-footer-left">
@@ -16,19 +30,19 @@ const Footer = () => {
         </div>
         <div className="o-follow-us">
         <h2>FOLLOW US</h2>
-        <div className="o-social-container">
+        <div className="o-social-container" onClick={()=>{redirectFace()}}>
           <FaFacebookF color='white'/>
         <h4>Facebook</h4>
         </div>
-        <div className="o-social-container">
+        <div className="o-social-container" onClick={()=>{redirectInst()}}>
           <FaInstagram color='white'/>
         <h4>Instagram</h4>
         </div>
-        <div className="o-social-container">
+        <div className="o-social-container" onClick={()=>{redirectTiwt()}}>
           <FaTwitter color='white'/>
         <h4>Twitter</h4>
         </div>
-        <div className="o-social-container">
+        <div className="o-social-container" onClick={()=>{redirectLinked()}}>
           <FaLinkedin color='white'/>
         <h4>LinkeIn</h4>
         </div>
