8905ca08aefe0cfa8b4986ac9b390b879d60a5c5
edsson cortes <edsson_andree.cortes@uao.edu.co>
2022-03-18 10:48:27 -0500

    responsive de la pantalla de locations

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,130 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import LocationCard from "../LocationCard/LocationCard";
 import "./Location.scss";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-/*import FormLabel from "@mui/material/FormLabel";*/
+import { IoIosArrowDown } from "react-icons/io";
+
 
 const Location = ({ all }) => {
+
+  const [desplegar, setDesplegar] = useState(false)
+
+  console.log(desplegar);
   return (
     <div className="o-location-container">
       <div className="o-location-left">
-       <h3>LOCATION</h3>
-       <input type="text" placeholder="Location"/>
+        <h3>LOCATION</h3>
+        <input type="text" placeholder="Location" />
+        <IoIosArrowDown className={desplegar ? `arrow-dissapear`:`arrow`} size='2rem' onClick={()=>setDesplegar(!desplegar)}/>
 
-          <FormControl>
-            <h3>DATE</h3>
-            <RadioGroup
-              aria-labelledby="dates"
-              defaultValue="Any day"
-              name="radio-dates"
+        <div className={desplegar ? `o-form-container-activo`: `o-form-container`}>
+          <h3>DATE</h3>
+          <form>
+            <label>
+              <input type="radio" name="radio" checked />
+              <span>Any day</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>Today</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>This week</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>This month</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>This year</span>
+            </label>
+          </form>
+          
+        </div>
 
-            >
-              <FormControlLabel
-                value="Any day"
-                control={<Radio size="small" />}
-                label="Any day"
-              />
-              <FormControlLabel
-                value="Today"
-                control={<Radio size="small"/>}
-                label="Today"
-              />
-
-              <FormControlLabel
-                value="This week"
-                control={<Radio size="small"/>}
-                label="This week"
-              />
-              <FormControlLabel
-                value="This month"
-                control={<Radio size="small"/>}
-                label="This month"
-              />
-              <FormControlLabel
-                value="This year"
-                control={<Radio size="small"/>}
-                label="This year"
-              />
-            </RadioGroup>
-          </FormControl>
-         
-          <FormControl>
+        <div className={desplegar ? `o-form-container-activo`: `o-form-container`}>
           <h3>PRICE</h3>
-            <RadioGroup
-              aria-labelledby="price"
-              defaultValue="Any price"
-              name="radio-price"
-            >
-              <FormControlLabel
-                value="Any price"
-                control={<Radio size="small" />}
-                label="Any price"
-              />
-              <FormControlLabel
-                value="Free"
-                control={<Radio size="small" />}
-                label="Free"
-              />
-              <FormControlLabel
-                value="Paid"
-                control={<Radio size="small" />}
-                label="Paid"
-              />
-              
-            </RadioGroup>
-          </FormControl>
+          <form>
+            <label>
+              <input type="radio" name="radio" checked />
+              <span>Any price</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>Free</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>Paid</span>
+            </label>
+            
+          </form>
+          
+        </div>
 
-          <FormControl>
+        <div className={desplegar ? `o-form-container-activo`: `o-form-container`}>
           <h3>FORMAT</h3>
-            <RadioGroup
-              aria-labelledby="format"
-              defaultValue="Any-format"
-              name="radio-format"
-            >
-              <FormControlLabel
-                value="Any-format"
-                control={<Radio size="small" />}
-                label="Any format"
-              />
-              <FormControlLabel
-                value="Festival"
-                control={<Radio size="small" />}
-                label="Festival"
-              />
-              <FormControlLabel
-                value="Convention"
-                control={<Radio size="small" />}
-                label="Convention"
-              />
-              <FormControlLabel
-                value="Expo"
-                control={<Radio size="small" />}
-                label="Expo"
-              />
-              <FormControlLabel
-                value="Conference"
-                control={<Radio size="small" />}
-                label="Conference"
-              />
-              <FormControlLabel
-                value="Seminar"
-                control={<Radio size="small" />}
-                label="Seminar"
-              />
-              
-            </RadioGroup>
-          </FormControl>
-       
+          <form>
+            <label>
+              <input type="radio" name="radio" checked />
+              <span>Any format</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>Festival</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>Convention</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>Expo</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>Conference</span>
+            </label>
+            <label>
+              <input type="radio" name="radio" />
+              <span>Seminar</span>
+            </label>
+            
+          </form>
+          
+        </div>
+        <IoIosArrowDown className={desplegar ? `arrow-active`:`arrow-dissapear`} size='2rem' onClick={()=>setDesplegar(!desplegar)}/>
       </div>
       <div className="o-location-middle">
         <LocationCard all={all} />
       </div>
       <div className="o-location-right">
         {/*OJO VERSION SOLO PARA PRUEBA, INTEGRAR MAP CON LA API OFICIAL DE GOOGLE*/}
-        <iframe title="iframe map"
+        <iframe
+          title="iframe map"
           className="imap"
           id="gmap_canvas"
           src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=17&ie=UTF8&iwloc=&output=embed"
